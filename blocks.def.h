e3a7b9811ad4bba30c654b39c9eca66c544ce1ab
papitz <paul@familie-hoeppner.de>
2023-10-13 15:16:16 -0700

    changed to tokyonight

--- blocks.def.h
+++ blocks.def.h
@@ -7,19 +7,19 @@ static const Block blocks[] = {
 // internet connection
 	/* {"",                            "~/.local/bin/dwmnet",		                                                5,			0}, */
 // cpu usage
-	{"^c#c678dd^ ^c#abb2bf^",      "top -b -n1 | grep \"Cpu(s)\" | awk '{printf \"%5.2f\%\", $1 + $2}'",		1,			0},
+	{"^c#c0caf5^ ^c#a9b1d6^",      "top -b -n1 | grep \"Cpu(s)\" | awk '{printf \"%5.2f\%\", $1 + $2}'",		1,			0},
 // ram usage
-	{"^c#c678dd^ ^c#abb2bf^",      "free -m | awk '/^Mem/ { print $3\"MB/\"$2\"MB\" }' | sed s/i//g",		    2,			0},
+	{"^c#c0caf5^ ^c#a9b1d6^",      "free -m | awk '/^Mem/ { print $3\"MB/\"$2\"MB\" }' | sed s/i//g",		    2,			0},
 // hdd usage
-	{"^c#c678dd^ ^c#abb2bf^",      "df -h | awk 'FNR == 5 {print $4\"B\"}'",					                10,			0},
+	{"^c#c0caf5^ ^c#a9b1d6^",      "df -h | awk 'FNR == 5 {print $4\"B\"}'",					                10,			0},
 // packages to update
-	{"^c#61afef^  ^c#abb2bf^",	    "checkupdates | wc -l | awk '{print $0 \" pkg\"}'",				            3600,		1},
+	{"^c#7aa2f7^  ^c#a9b1d6^",	    "checkupdates | wc -l | awk '{print $0 \" pkg\"}'",				            3600,		1},
 // date
-	{"^c#d19a66^ ^c#abb2bf^", 	    "date '+%a, %d. %b' ",								                        5,			0},
+	{"^c#e0af68^ ^c#a9b1d6^", 	    "date '+%a, %d. %b' ",								                        5,			0},
 // clock
-	{"^c#d19a66^ ^c#abb2bf^",      "date '+%_H:%M'  ",  								                        5,			0},
+	{"^c#e0af68^ ^c#a9b1d6^",      "date '+%_H:%M'  ",  								                        5,			0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "^c#56B6C2^ | ";
+static char delim[] = "^c#7dcfff^ | ";
 static unsigned int delimLen = 50;
